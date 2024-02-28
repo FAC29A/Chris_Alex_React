de1844f40f84c0c40022dd6800c0efa142242aa3
Alexander <alex@alexvoiceover.com>
2024-02-28 18:38:44 +0000

    initilise branch to create server

--- model/tasksFunctions.d.ts
+++ model/tasksFunctions.d.ts
@@ -1,8 +1,8 @@
 // Import the Task interface from the TaskList.tsx file
-import { Task } from './components/TaskList';
+import { Task } from './components/TaskList'
 
 // Define the function signatures
-export function createTask(task: TaskInput): Task; // Assuming this returns a Task object
-export function getTasks(): Task[]; // Assuming this returns an array of Task objects
-export function deleteTask(id: number): void; // Assuming this doesn't return anything
-export function editTask(id: number): Task; // Assuming this returns a Task object
+export function createTask(task: Task): Task // Assuming this returns a Task object
+export function getTasks(): Task[] // Assuming this returns an array of Task objects
+export function deleteTask(id: number): void // Assuming this doesn't return anything
+export function editTask(id: number): Task // Assuming this returns a Task object
